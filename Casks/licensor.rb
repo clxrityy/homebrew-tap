3d75f435cb3558677a10331017e111de05230255
clxrityy <97744702+clxrityy@users.noreply.github.com>
2026-06-16 15:43:47 -0700

    (chore) update licensor version

--- Casks/licensor.rb
+++ Casks/licensor.rb
@@ -1,5 +1,5 @@
 cask "licensor" do
-    version "0.3.0"
+    version "0.3.1"
     sha256 "d41c9ea7607594fae38f8f879ddb50941218a3e4f586a42d2ad45fa05b41b576"
 
     url "https://github.com/clxrityy/Licensor/releases/download/v#{version}/Licensor_#{version}_universal.dmg"
