2dea7b1f738e034d8e23c73e2e054b7dffa1d0f6
clxrityy <97744702+clxrityy@users.noreply.github.com>
2026-06-15 23:05:21 -0700

    use correct sha

--- Formula/rawget.rb
+++ Formula/rawget.rb
@@ -4,7 +4,7 @@ class Rawget < Formula
   desc "Lightweight CLI tool for downloading files from URLs"
   homepage "https://github.com/clxrityy/rawget"
   url "https://files.pythonhosted.org/packages/95/3a/4b9f3b6f65728b9bbb95d452d7e523f85262df2578c950ec4097784e05f5/rawget-0.11.1.tar.gz"
-  sha256 "09edf626bcf57cc4705d992d9c626f822f2e07535e76b6174f675b7aa5a2be14"
+  sha256 "95cc14e106b4ef26aa5ee947f7d35758f5565cf5db29093ad126d260ec203889"
   license "MIT"
 
   depends_on "python@3.14"
