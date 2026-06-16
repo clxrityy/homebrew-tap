e044bb996aa053d6bc283095a41d69062632863b
clxrityy <97744702+clxrityy@users.noreply.github.com>
2026-06-16 15:42:43 -0700

    (chore) update rawget version

--- Formula/rawget.rb
+++ Formula/rawget.rb
@@ -3,8 +3,8 @@ class Rawget < Formula
 
   desc "Lightweight CLI tool for downloading files from URLs"
   homepage "https://github.com/clxrityy/rawget"
-  url "https://files.pythonhosted.org/packages/95/3a/4b9f3b6f65728b9bbb95d452d7e523f85262df2578c950ec4097784e05f5/rawget-0.11.1.tar.gz"
-  sha256 "95cc14e106b4ef26aa5ee947f7d35758f5565cf5db29093ad126d260ec203889"
+  url "https://files.pythonhosted.org/packages/4c/f9/df56df98fc83fe723d4e9d63c94a065e82127af2522971d60fded704b184/rawget-0.11.2.tar.gz"
+  sha256 "70eeb920382726bc8e919b3de94df660f70f9a73b6f75dae0c459f5555db51b3"
   license "MIT"
 
   depends_on "python@3.14"
